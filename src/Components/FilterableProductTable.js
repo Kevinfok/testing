a0b42b8452b87535bd4dfe6a145704a5212aeba5
Kevinfok <fcw0821@gmail.com>
2022-03-07 22:47:40 +0800

    completed

--- src/Components/FilterableProductTable.js
+++ src/Components/FilterableProductTable.js
@@ -16,17 +16,29 @@ const FilterableProductTable = (props) => {
     return sortedProduct;
   }
   const controlSearchDataHandler = (enteredData) => {
+    if (enteredData.checked === false) {
+      const result = props.Product.filter((list) =>
+        list.name
+          .toLowerCase()
+          .includes(enteredData.enteredProduct.toLowerCase())
+      );
+      
+      setProduct(() => {
+        return result;
+      });
+    } else {
+      const result = props.Product.filter(
+        (list) =>
+          list.name
+            .toLowerCase()
+            .includes(enteredData.enteredProduct.toLowerCase()) &&
+          list.stocked === true
+      );
 
-    const result = props.Product.filter((list) =>
-    
-      list.name.toLowerCase().includes(enteredData.enteredProduct.toLowerCase())
-    );
-
-
-    setProduct(() => {
-      return result;
-    });
-
+      setProduct(() => {
+        return result;
+      });
+    }
   };
 
   return (
